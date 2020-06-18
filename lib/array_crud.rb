367e772b40ebd8e9f0b9278f0ef84835fd394f2a
Carlos Rosario <carlosrosario2k@gmail.com>
2020-06-18 20:43:53 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -31,8 +31,8 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  elements = ["wow", "Shoulders", "Knees", "Toes"]
-  elements[2]
+  elements = ["wow", "am", "Knees", "Toes"]
+  elements[1]
 
 end
 
