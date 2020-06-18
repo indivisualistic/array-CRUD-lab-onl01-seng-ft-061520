a85f13978261f05b93c69e2e98370630311852ec
Carlos Rosario <carlosrosario2k@gmail.com>
2020-06-18 20:26:43 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -14,8 +14,8 @@ end
 
 
 def add_element_to_start_of_array(array, element)
+  elements = ["Heads", "Shoulders", "Knees", "Toes"]
   elements.unshift("arms")
-
 end
 
 def remove_element_from_end_of_array(array)
